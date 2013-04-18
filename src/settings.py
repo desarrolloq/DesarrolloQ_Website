b50b857d89bf81a47e0a7ecce5c4dec675a2b6ed
dionys <dionyself@gmail.com>
2013-04-18 16:33:38 -0400

    added template & update directories

--- src/settings.py
+++ src/settings.py
@@ -22,7 +22,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.desarrolloq.com','localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -49,27 +49,27 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/media/media.desarrolloq.com/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://desarrolloq.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/media/media.desarrolloq.com/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://media.desarrolloq.com/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = 'http://media.desarrolloq.com/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -110,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/home/html/django_templates"
 )
 
 INSTALLED_APPS = (
--- src/urls.py
+++ src/urls.py
@@ -10,7 +10,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DesarrolloQ_Website.views.home', name='home'),
-    # url(r'^DesarrolloQ_Website/', include('DesarrolloQ_Website.foo.urls')),
+    #url(r'^DesarrolloQ_Website/', include('DesarrolloQ_Website.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -18,4 +18,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^$',  ListView.as_view(model=Caption, template_name='index.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$','django.views.static.serve', {'document_root': '/media/media.desarrolloq.com/static/'}),
 )
